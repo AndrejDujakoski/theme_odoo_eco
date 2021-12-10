1098d94e3655c13571e1debfdfc25e6d45759252
AndrejDujakoski <93519101+AndrejDujakoski@users.noreply.github.com>
2021-12-10 09:43:08 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'onboarding_theme',
+    'description': 'Learing to build a theme as part of '
+                   'the onboarding for SimplifyERP',
+    'version': '1.0',
+    'author': 'Andrej Dujakoski',
+    'category': 'Theme/Website',
+
+    'depends': ['website'],
+    'data': [
+        'views/odoo_theme.xml',
+        'views/extend_header.xml',
+        'views/services_page.xml',
+        'views/assets_link_css.xml',
+        'views/snippets.xml',
+        'views/snippets_options.xml',
+        'views/jumbo_snippet.xml',
+        'views/jumboTwo_snippet.xml',
+        'views/green_snippet.xml',
+        'views/cover_snippet.xml',
+        'views/text_image_snippet.xml',
+        # 'views/green_energy_snippet.xml',
+
+    ],
+}
